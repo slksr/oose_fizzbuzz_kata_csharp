49fa17d71fe61fac4b7b1033a3b5b198b073d9d9
Ronald <ronald.schellekens@han.nl>
2022-09-18 12:01:23 +0200

    RandomNameGenerator en test

---
+++ src/OOSE.FizzBuzzKata.FizzBuzzLogic/NameGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSE.FizzBuzzKata.FizzBuzzLogic
+{
+    public class NameGenerator
+    {
+        private Random random = new Random();
+        private string[] names;
+
+        public NameGenerator()
+        {
+            SetNames();
+        }
+
+        public virtual string GenerateRandomName()
+        {
+            return names[random.Next(names.Length)];
+        }
+
+        private void SetNames()
+        {
+            names = new string[] { "Donald", "Daffy", "Minnie", "Dagobert", "Kwik", "Kwek", "Kwak", "Guus"};
+        }
+
+
+    }
+}
---
+++ src/OOSE.FizzBuzzKata.Tests/NameGeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOSE.FizzBuzzKata.FizzBuzzLogic;
+
+namespace OOSE.FizzBuzzKata.Tests
+{
+    public class NameGeneratorTest
+    {
+        [Fact]
+        public void GenerateRandomNameTest()
+        {
+            NameGenerator generator = new NameGenerator();
+            var actValue = generator.GenerateRandomName();
+            Assert.NotEmpty(actValue);
+        }
+    }
+}
